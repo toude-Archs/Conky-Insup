f3eeb6d74c02b9e40811ace2b58f9aefd4743386
Medhi Michel <100583551+toude-Archs@users.noreply.github.com>
2023-02-28 21:31:01 +0100

    Add files via upload

---
+++ toude-conky-insup.sh
@@ -0,0 +1,17 @@
+cd /home/$USER/
+
+git clone https://github.com/toude-Archs/My-Conky.git
+
+cd My-Conky/
+
+mv conkyrc /home/$USER
+
+cd /home/$USER
+
+mv conkyrc .conkyrc
+
+rm -rf My-Conky.zip
+
+rm -rf My-conky
+
+echo Enjoy My conky as ben Installed, Just Change Your CPU model ! Now Restart Conky
